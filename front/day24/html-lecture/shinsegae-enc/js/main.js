23e8cac133896c0595dfa3f793c4384ea0ae97ff
nou13n8e <nou13n8e@gmail.com>
2023-05-26 18:18:08 +0900

    0526

---
+++ front/day24/html-lecture/shinsegae-enc/js/main.js
@@ -0,0 +1,23 @@
+Splitting();
+AOS.init();
+
+const header = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const scrollY = window.scrollY;
+  if (scrollY > 0) {
+    header.classList.add("on");
+  } else {
+    header.classList.remove("on");
+  }
+});
+
+const mainSwiper = new Swiper(".main-mask", {
+  loop: true,
+});
+
+const midSwiper = new Swiper(".mid-mask", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 100,
+});
