ded5baa608bc573b35311d083dcb62f2c87a4b38
nou13n8e <nou13n8e@gmail.com>
2023-06-01 18:21:00 +0900

    0601

--- front/day27/javascript/map/js/camping.js
+++ front/day27/javascript/map/js/camping.js
@@ -13,6 +13,13 @@ const clusterer = new kakao.maps.MarkerClusterer({
 });
 //7 clusterer 활용을 위해 clusterer 선언 및 속성 입력
 
+// 마커 위에 커스텀 오버레이를 표시합니다.
+const overlay = new kakao.maps.CustomOverlay({
+  // content: content,
+  // map: map,
+  // position: marker.getPosition(),
+});
+
 const searchCamping = async () => {
   //3 searchCamping이라는 함수 정의
   const camping = await fetch(
@@ -33,6 +40,31 @@ const searchCamping = async () => {
       map: map,
       position: new kakao.maps.LatLng(item.mapY, item.mapX),
     });
+
+    // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다.
+    kakao.maps.event.addListener(marker, "click", function () {
+      overlay.setMap(map);
+      overlay.setPosition(marker.getPosition());
+      overlay.setContent(
+        // 커스텀 오버레이에 표시할 컨텐츠 입니다
+        // 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+        // 별도의 이벤트 메소드를 제공하지 않습니다
+        `<div class="wrap">
+              <div class="info">
+                  <div class="title">
+                      ${item.facltNm}
+                      <div class="close" onclick="closeOverlay()" title="닫기"></div>
+                  </div>
+                  <div class="body">
+                      <div class="desc">
+                          <div class="ellipsis">${item.addr1}</div>
+                      </div>
+                  </div>
+              </div>
+          </div>`
+      );
+    });
+    map.setCenter(marker.getPosition());
     markers.push(marker);
     //9 markers(배열)에 반복되고 있는 marker를 push하게 설정
   });
@@ -42,3 +74,8 @@ const searchCamping = async () => {
 
 searchCamping();
 //함수는 정의하고 난 뒤 꼭 호출할 것!
+
+// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다.
+function closeOverlay() {
+  overlay.setMap(null);
+}
